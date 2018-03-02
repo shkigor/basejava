220e02f4f509dffea4f5dd07a780ae97f6b51898
Igor Shklyar <igor.shklyar@gmail.com>
2018-03-02 21:21:48 +0200

    ArrayStorage: implement clear(), delete(), getAll() and size() methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,6 +7,9 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
@@ -27,16 +30,37 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (Objects.nonNull(storage[i]) && storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        final int resultStorageSize = size();
+        Resume[] resultStorage = new Resume[resultStorageSize];
+        int index = 0;
+
+        for (Resume r : storage) {
+            if (Objects.nonNull(r)) {
+                resultStorage[index] = r;
+                index++;
+            }
+        }
+        return resultStorage;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume r : storage) {
+            if (Objects.nonNull(r)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
