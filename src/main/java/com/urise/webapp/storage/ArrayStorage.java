c138adb396e76559f448b3fa5ede5a5f21308bf1
Igor Shklyar <igor.shklyar@gmail.com>
2018-03-26 22:48:34 +0300

    Lesson03 Return to HW02 commit with some changes

--- src/main/java/com/urise/webapp/storage/ArrayStorage.java
+++ src/main/java/com/urise/webapp/storage/ArrayStorage.java
@@ -4,7 +4,6 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Array based storage for Resumes
@@ -14,7 +13,6 @@ public class ArrayStorage {
     private final int NOT_FOUND = -1;
     private Resume[] storage = new Resume[10000];
     private int size;
-    private int index = NOT_FOUND;
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -23,23 +21,20 @@ public class ArrayStorage {
 
     public void update(Resume resume) {
         Objects.requireNonNull(resume, "ERROR update. Resume cannot be null");
-
-        if (getIndex(resume.getUuid()).isPresent()) {
-            storage[index] = resume;
+        int index = getIndex(resume.getUuid());
+        if (index == NOT_FOUND) {
+            System.out.println("Resume " + resume.getUuid() + " not exist");
         } else {
-            System.out.println("Resume(" + resume.getUuid() + ") not exist");
+            storage[index] = resume;
         }
     }
 
     public void save(Resume resume) {
         Objects.requireNonNull(resume, "ERROR save. Resume cannot be null");
-        if (size == storage.length) {
+        if (getIndex(resume.getUuid()) != NOT_FOUND) {
+            System.out.println("Resume " + resume.getUuid() + " already exist");
+        } else if (size >= storage.length) {
             System.out.println("Storage overflow");
-            return;
-        }
-
-        if (getIndex(resume.getUuid()).isPresent()) {
-            System.out.println("Resume(" + resume.getUuid() + ") already exist");
         } else {
             storage[size] = resume;
             size++;
@@ -48,46 +43,46 @@ public class ArrayStorage {
 
     public Resume get(String uuid) {
         Objects.requireNonNull(uuid, "ERROR get. uuid cannot be null");
-
-        if (getIndex(uuid).isPresent()) {
-            return storage[index];
-        } else {
-            System.out.println("Resume(" + uuid + ") not exist");
+        int index = getIndex(uuid);
+        if (index == NOT_FOUND) {
+            System.out.println("Resume " + uuid + " not exist");
             return null;
         }
+        return storage[index];
     }
 
     public void delete(String uuid) {
         Objects.requireNonNull(uuid, "ERROR delete. uuid cannot be null");
-
-        if (getIndex(uuid).isPresent()) {
+        int index = getIndex(uuid);
+        if (index == NOT_FOUND) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
             size--;
             storage[index] = storage[size];
             storage[size] = null;
-        } else {
-            System.out.println("Resume(" + uuid + ") not exist");
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+        Resume[] result = new Resume[size];
+        System.arraycopy(storage, 0, result, 0, size);
+        return result;
     }
 
     public int size() {
         return size;
     }
 
-    private Optional<Integer> getIndex(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                index = i;
-                return Optional.of(i);
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
             }
         }
-        index = NOT_FOUND;
-        return Optional.empty();
+        return NOT_FOUND;
     }
 }
