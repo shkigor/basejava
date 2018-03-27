9a2c5696e838b30f489bb953ee43b1ecfa8e0e7a
Igor Shklyar <igor.shklyar@gmail.com>
2018-03-27 16:09:12 +0300

    Lesson03 implements interface

--- src/main/java/com/urise/webapp/storage/ArrayStorage.java
+++ src/main/java/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+public class ArrayStorage implements Storage {
 
     private static final int STORAGE_LIMIT = 10000;
     private final int NOT_FOUND = -1;
