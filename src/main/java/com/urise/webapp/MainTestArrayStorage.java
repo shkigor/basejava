36fb806b6c60df324d2562547bcb2370453ecc00
Igor Shklyar <igor.shklyar@gmail.com>
2018-03-27 16:44:19 +0300

    Lesson03 use interface and final

--- src/main/java/com/urise/webapp/MainTestArrayStorage.java
+++ src/main/java/com/urise/webapp/MainTestArrayStorage.java
@@ -2,19 +2,20 @@ package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.storage.Storage;
 
 /**
  * Test for com.urise.webapp.storage.ArrayStorage
  */
 public class MainTestArrayStorage {
-    private static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        final Resume r1 = new Resume();
         r1.setUuid("uuid1");
-        Resume r2 = new Resume();
+        final Resume r2 = new Resume();
         r2.setUuid("uuid2");
-        Resume r3 = new Resume();
+        final Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
         ARRAY_STORAGE.save(r1);
