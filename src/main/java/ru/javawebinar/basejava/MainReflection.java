142fff0bbc67584abf7dac980554cd4c353a2dd1
Igor Shklyar <igor.shklyar@gmail.com>
2018-06-04 23:00:20 +0300

    Ex.04: Invocation of Resume.toString() method via reflection was added

--- src/main/java/ru/javawebinar/basejava/MainReflection.java
+++ src/main/java/ru/javawebinar/basejava/MainReflection.java
@@ -3,17 +3,23 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
-        Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class resumeClass = Resume.class;
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
-        System.out.println(field.get(r));
-        field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        Resume resume = new Resume();
+        System.out.println(field.get(resume));
+        field.set(resume, "new_uuid");
+        System.out.println(resume);
+
+        // invoke resume.toString via reflection
+        Method methodToString = resumeClass.getMethod("toString", null);
+        System.out.println(methodToString.invoke(resume));
     }
 }
