6574d864ee8668e0da5746d52d6b3be1b781a889
Igor Shklyar <igor.shklyar@gmail.com>
2018-06-20 16:53:56 +0300

    Added static init block

--- src/test/java/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ src/test/java/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -16,16 +16,21 @@ public abstract class AbstractArrayStorageTest {
     protected final Storage storage;
 
     private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-
     private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-
     private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-    
     protected static final String NOT_EXIST_UUID = "NOT_EXIST_UUID";
-    private static final Resume RESUME_NOT_EXIST = new Resume(NOT_EXIST_UUID);
+
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+    private static final Resume RESUME_NOT_EXIST;
+
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_NOT_EXIST = new Resume(NOT_EXIST_UUID);
+    }
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
