e7716d9a56c59fab359d701440ed807a3c421fbf
Igor Shklyar <igor.shklyar@gmail.com>
2018-07-10 15:11:47 +0300

    HW05: MapStorage was added.

--- src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
+++ src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
@@ -1,21 +1,83 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class MapStorage extends AbstractStorage {
+
+    private Map<String, Resume> resumeMap = new HashMap<>();
+
     @Override
     public void clear() {
+        resumeMap.clear();
+        size = 0;
+    }
 
+    @Override
+    public void update(Resume resume) {
+        Objects.requireNonNull(resume, "ERROR update. Resume cannot be null");
+
+        Resume r = resumeMap.get(resume.getUuid());
+        if (Objects.isNull(r)) {
+            throw new NotExistStorageException(resume.getUuid());
+        }
+        resumeMap.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    public void save(Resume resume) {
+        Objects.requireNonNull(resume, "ERROR save. Resume cannot be null");
+
+        Resume r = resumeMap.get(resume.getUuid());
+        if (Objects.nonNull(r)) {
+            throw new ExistStorageException(resume.getUuid());
+        } else if (size >= STORAGE_LIMIT) {
+            throw new StorageException("Storage overflow", resume.getUuid());
+        } else {
+            resumeMap.put(resume.getUuid(), resume);
+            size++;
+        }
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        Objects.requireNonNull(uuid, "ERROR get. uuid cannot be null");
+
+        Resume r = resumeMap.get(uuid);
+        if (Objects.isNull(r)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return r;
+    }
+
+    @Override
+    public void delete(String uuid) {
+        Objects.requireNonNull(uuid, "ERROR delete. uuid cannot be null");
+
+        Resume r = resumeMap.get(uuid);
+        if (Objects.isNull(r)) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            size--;
+            resumeMap.remove(uuid);
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        return resumeMap.values().toArray(resumes);
     }
 
     @Override
     protected void add(int index, Resume resume) {
-
+        resumeMap.put(resume.getUuid(), resume);
     }
 
     @Override
---
+++ src/test/java/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.basejava.storage;
+
+public class MapStorageTest extends AbstractStorageTest {
+
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
+}
