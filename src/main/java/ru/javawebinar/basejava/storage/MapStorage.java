a60f4acf075a682f96b4da7f681e53dc3d1a6eb4
Igor Shklyar <igor.shklyar@gmail.com>
2018-07-17 16:26:07 +0300

    HW05: Empty MapStorage to leave only stubs

--- src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
+++ src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
@@ -1,12 +1,9 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.ExistStorageException;
-import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class MapStorage extends AbstractStorage {
 
@@ -18,60 +15,14 @@ public class MapStorage extends AbstractStorage {
     }
 
     @Override
-    public void delete(String uuid) {
-        Objects.requireNonNull(uuid, "ERROR delete. uuid cannot be null");
-
-        Resume r = resumeMap.get(uuid);
-        if (Objects.isNull(r)) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            resumeMap.remove(uuid);
-        }
-    }
-
-    @Override
-    public Resume get(String uuid) {
-        Objects.requireNonNull(uuid, "ERROR get. uuid cannot be null");
-
-        Resume r = resumeMap.get(uuid);
-        if (Objects.isNull(r)) {
-            throw new NotExistStorageException(uuid);
-        }
-        return r;
-    }
-
-    @Override
     public Resume[] getAll() {
         Resume[] resumes = new Resume[size()];
         return resumeMap.values().toArray(resumes);
     }
 
     @Override
-    public void save(Resume resume) {
-        Objects.requireNonNull(resume, "ERROR save. Resume cannot be null");
-
-        Resume r = resumeMap.get(resume.getUuid());
-        if (Objects.nonNull(r)) {
-            throw new ExistStorageException(resume.getUuid());
-        } else {
-            resumeMap.put(resume.getUuid(), resume);
-        }
-    }
-
-    @Override
     public int size() {
         return resumeMap.size();
-    }
-
-    @Override
-    public void update(Resume resume) {
-        Objects.requireNonNull(resume, "ERROR update. Resume cannot be null");
-
-        Resume r = resumeMap.get(resume.getUuid());
-        if (Objects.isNull(r)) {
-            throw new NotExistStorageException(resume.getUuid());
-        }
-        resumeMap.put(resume.getUuid(), resume);
     }
 
     @Override
