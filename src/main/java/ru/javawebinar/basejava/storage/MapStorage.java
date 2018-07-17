bb8a304f43dbec97568b570b96f78a95d0602358
Igor Shklyar <igor.shklyar@gmail.com>
2018-07-17 17:44:38 +0300

    HW05: Removed unnecessary implementation

--- src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
+++ src/main/java/ru/javawebinar/basejava/storage/MapStorage.java
@@ -18,44 +18,9 @@ public class MapStorage extends AbstractStorage {
     }
 
     @Override
-    public void delete(String uuid) {
-        Objects.requireNonNull(uuid, "ERROR delete. uuid cannot be null");
-
-        Resume r = resumeMap.get(uuid);
-        if (Objects.isNull(r)) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            resumeMap.remove(uuid);
-        }
-    }
-
-    @Override
-    public Resume get(String uuid) {
-        Objects.requireNonNull(uuid, "ERROR get. uuid cannot be null");
-
-        Resume r = resumeMap.get(uuid);
-        if (Objects.isNull(r)) {
-            throw new NotExistStorageException(uuid);
-        }
-        return r;
-    }
-
-    @Override
     public Resume[] getAll() {
         Resume[] resumes = new Resume[size()];
         return resumeMap.values().toArray(resumes);
-    }
-
-    @Override
-    public void save(Resume resume) {
-        Objects.requireNonNull(resume, "ERROR save. Resume cannot be null");
-
-        Resume r = resumeMap.get(resume.getUuid());
-        if (Objects.nonNull(r)) {
-            throw new ExistStorageException(resume.getUuid());
-        } else {
-            resumeMap.put(resume.getUuid(), resume);
-        }
     }
 
     @Override
@@ -72,29 +37,13 @@ public class MapStorage extends AbstractStorage {
     }
 
     @Override
-    public void update(Resume resume) {
-        Objects.requireNonNull(resume, "ERROR update. Resume cannot be null");
-
-        Resume r = resumeMap.get(resume.getUuid());
-        if (Objects.isNull(r)) {
-            throw new NotExistStorageException(resume.getUuid());
-        }
-        resumeMap.put(resume.getUuid(), resume);
-    }
-
-    @Override
     protected Object getIndex(String uuid) {
         return uuid;
     }
 
     @Override
     protected Resume getResumeByIndex(Object indexObj) {
-        Resume resume = resumeMap.get(indexObj);
-        if (Objects.isNull(resume)) {
-            throw new NotExistStorageException("TODO");
-//            throw new NotExistStorageException(uuid);
-        }
-        return resume;
+        return resumeMap.get(indexObj);
     }
 
     @Override
